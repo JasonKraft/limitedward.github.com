21117af68c1a638a12c822d3995e4580d0f1e23b
Jason Kraft <uberrewrite@gmail.com>
2013-02-05 19:41:31 -0500

    401

--- Haxlet/haxlet.js
+++ Haxlet/haxlet.js
@@ -39,7 +39,9 @@
 			// your JavaScript code goes here!
 			
 			$('body').append('<div id="scoreboard"></div>');
-			$('#scoreboard').html('        	<div id="topBar">Haxlet Scoreboard 1.0 - Copyright 2013 Jason Kraft <span id="announcer"></span><span id="swapper">SWAP TEAM</span></div>        	<div id="scoreleft" contenteditable="true">Blue Team</div>                <div id="scorecenter">        	        	<div id="bluescore">            	<div id="incrementb">&#8743;</div>                <div id="scorepieceb">0</div>                <div id="decrementb">&#8744;</div>            </div>                        <div id="timer">            	<div id="pause"><a href="#">Play/Pause</a></div>            	<div id="timepiece">5:00</div>                <div id="reset"><a href="#">Reset</a></div>            </div>                        <div id="redscore">            	<div id="incrementr">&#8743;</div>                <div id="scorepiecer">0</div>                <div id="decrementr">&#8744;</div>            </div>                    </div>                <div id="scoreright" contenteditable="true">Red Team</div>            ');
+			$('#scoreboard').html('        	<div id="topBar">Haxlet Scoreboard 1.0 - Copyright 2013 Jason Kraft <span id="announcer"></span><span id="swapper">SWAP TEAM</span></div>        	<div id="scoreleft" contenteditable="true">Blue Team</div>                <div id="scorecenter">        	        	<div id="bluescore">            	<div id="incrementb">&#8743;</div>                <div id="scorepieceb">0</div>                <div id="decrementb">&#8744;</div>            </div>                        <div id="timer">            	<div id="pause"><a href="#">Play/Pause</a></div>            	<div id="timepiece">5:00</div>                <div id="reset"><a href="#">Reset</a></div>            </div>                        <div id="redscore">            	<div id="incrementr">&#8743;</div>                <div id="scorepiecer">0</div>                <div id="decrementr">&#8744;</div>            </div>                    </div>                <div id="scoreright" contenteditable="true">Red Team</div>            ').ready(function(e) {
+                
+            
 			//$('#scoreboard').draggable();
 
 $('head').append('<style type="text/css">\
@@ -214,7 +216,7 @@ $('head').append('<style type="text/css">\
 	}\
 </style>');
 
-$('#scoreboard').ready(function(){var blueScore = 0;
+var blueScore = 0;
 var redScore = 0;
 var paused = false;
 
